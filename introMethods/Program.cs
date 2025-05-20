ff790d5a918ad02a5b307bbf41eb6915dd3a4e0d
Mostafa Said <m.ssaid356@gmail.com>
2025-05-21 02:32:38 +0300

    Add files via upload

---
+++ introMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace IntroMethodsFuncs
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a= 1;
+            int b= 2;
+            Console.WriteLine("Before Swapping---");
+            Console.WriteLine($"A value: {a}");
+            Console.WriteLine($"B value: {b}");
+            
+            (a,b)= Swap(a,b);
+            Console.WriteLine("After Swapping---");
+            Console.WriteLine($"A value: {a}");
+            Console.WriteLine($"B value: {b}");
+
+        }
+        static (int,int) Swap(int a,int b)
+        {
+           int temp=a;
+            a=b;
+            b=temp;
+            return (a, b);
+        }
+    }
+}
+
+
+
+
+
+
+
+
